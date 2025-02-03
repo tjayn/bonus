1b8d3058b4721da5925615b965dbe3a7f9be3eb3
tjayn <150569644+tjayn@users.noreply.github.com>
2025-02-03 07:39:47 +0300

    Create MainApp.java

---
+++ MainApp.java
@@ -0,0 +1,63 @@
+//importing the scanner input class
+import java.util.Scanner;
+
+//Declaring Employee class
+class Employee {
+    String name;
+    double salary;
+
+    Employee (String name, double salary){
+        this.name = name;
+        this.salary = salary;
+    }
+    //method to display  Employee details
+    void displayDetails(){
+        System.out.println("Employee: " + this.name );
+        System.out.println("Salary: " + this.salary );
+
+    }
+    }
+    //start of the salary calculator class
+    class SalaryCalculator{
+        double salary;
+
+        //salary constructor
+        SalaryCalculator(double salary){
+            this.salary = salary;
+        }
+        // bonus calculation
+        double calculateBonus(){
+            return this.salary  * 0.1;
+        }
+
+    }
+public class MainApp {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        //input of name from user
+        System.out.print("Enter Employee's Name: ");
+        String name = scanner.nextLine();
+
+        //input of salary from user
+        System.out.print("Enter The Salary: ");
+        double salary = scanner.nextDouble();
+
+        //Employee object
+        Employee employee = new Employee(name, salary);
+        //SalaryCalculator object
+        SalaryCalculator calc = new SalaryCalculator(salary);
+
+        //calling of the method that will output the employee details
+        employee.displayDetails();
+
+        //the variable bonus takes the return ftom the bonus calculations
+        double bonus = calc.calculateBonus();
+
+        //output of the bonus
+        System.out.println("Bonus: " + bonus);
+
+
+    }
+}
